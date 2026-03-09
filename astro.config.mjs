da6339308052b4c06500f8f75c2c66db243a2add
Yasin <wytm97@protonmail.com>
2026-03-10 00:28:19 +0100

    fix: CNAME and astro config site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://envshq.github.io',
+	site: 'https://envsh.dev',
 	integrations: [
 		starlight({
 			title: 'envsh',
